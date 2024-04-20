a7d02b704db194ca92bb0c8cc9bea88920ebea8f
Daphne-Hsueh <daphnewk23@gmail.com>
2024-04-21 00:22:33 +0800

    feat: add basic UI for contactPage

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,21 +1,102 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import {
+  Box,
+  Container,
+  CssBaseline,
+  TextareaAutosize,
+  Typography,
+} from '@mui/material'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Carousel from 'react-material-ui-carousel'
+
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import SideDrawer from '../../components/Drawer'
+
+const DUMMY_PICS = [
+  'https://picsum.photos/id/10/400/300',
+  'https://picsum.photos/id/20/400/300',
+  'https://picsum.photos/id/30/400/300',
+]
 
 const ContactPage = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <Header />
-      Contact Page
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          minHeight: '96vh',
+        }}
+      >
+        <CssBaseline />
+        <Header />
+        <SideDrawer sections={null} />
+        <Box
+          component={'main'}
+          sx={{
+            p: 3,
+            marginTop: '60px',
+            width: '100%',
+          }}
+        >
+          <Typography variant="h5" textAlign={'center'}>
+            leave a message?
+          </Typography>
+          <Container
+            sx={{
+              width: '100%',
+              display: { sx: 'block', md: 'flex' },
+              justifyContent: 'center',
+            }}
+          >
+            <form style={{ width: '100%', marginTop: '30px' }}>
+              <TextField
+                label="Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Email"
+                variant="outlined"
+                sx={{ display: 'block' }}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Leave your message here!"
+                multiline
+                rows={8}
+                margin="normal"
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+            </form>
+            <Carousel
+              IndicatorIcon
+              navButtonsAlwaysInvisible
+              sx={{
+                width: '100%',
+                m: '30px',
+              }}
+            >
+              {DUMMY_PICS.map((image, index) => (
+                <img
+                  alt="contactPageImg"
+                  src={image}
+                  key={index}
+                  width={600}
+                  style={{ margin: '0 auto' }}
+                ></img>
+              ))}
+            </Carousel>
+          </Container>
+        </Box>
+      </Box>
       <Footer />
-    </Box>
+    </>
   )
 }
 
